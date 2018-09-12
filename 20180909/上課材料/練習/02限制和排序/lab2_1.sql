0955cc8fe700583d049d0791e716b6d378faca13
tom750407 <tom750407@hotmail.com>
2018-09-12 10:45:11 +0800

    exersice2

--- 20180909/上課材料/練習/02限制和排序/lab2_1.sql
+++ 20180909/上課材料/練習/02限制和排序/lab2_1.sql
@@ -1,1 +1,3 @@
-SELECT last_name,salary FROM employees WHERE salary > 12000;
+SELECT last_name,salary 
+FROM employees 
+WHERE salary > 12000;
--- 20180909/上課材料/練習/02限制和排序/lab2_2.sql
+++ 20180909/上課材料/練習/02限制和排序/lab2_2.sql
@@ -1,1 +1,3 @@
-SELECT last_name, department_id FROM employees WHERE employee_id = 176;
+SELECT last_name, department_id 
+FROM employees 
+WHERE employee_id = 176;
