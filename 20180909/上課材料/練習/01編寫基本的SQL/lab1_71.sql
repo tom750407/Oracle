ef1760421c90c1d39eb27d319d57310f5c66546d
tom750407 <tom750407@hotmail.com>
2018-09-14 15:19:09 +0800

    exercise1

---
+++ 20180909/上課材料/練習/01編寫基本的SQL/lab1_71.sql
@@ -0,0 +1,2 @@
+SELECT employee_id "Emp #", first_name "Employees", job_id "Job", hire_date "Hire Date" 
+FROM employees;
---
+++ 20180909/上課材料/練習/01編寫基本的SQL/lab1_91.sql
@@ -0,0 +1,5 @@
+SELECT employee_id || ',' || first_name || ',' || last_name || ',' 
+       || email || ',' || phone_number || ',' || hire_date || ',' 
+       || job_id || ',' || salary || ',' || commission_pct || ',' 
+       || manager_id || ',' || department_id the_output 
+FROM employees;
