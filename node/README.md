825e6b9c17a56a0fbe24c3f3536567758278e9a7
tom750407 <tom750407@hotmail.com>
2018-10-18 10:54:17 +0800

    Node study

---
+++ node/README.md
@@ -0,0 +1,10 @@
+# NODE - learnyounode
+
+### Exercise 1 - Hello World
+---
+--- Task ---
+Write a program that prints the text "HELLO WORLD" to the console (stdout).
+---
+
+
+
---
+++ node/test.js
@@ -0,0 +1,8 @@
+process.stdout.write("Hello World");
+process.stdout.write("Test");
+process.stdout.write("\n");
+console.log("------------------");
+console.log("Hello","World");
+console.log("------------------");
+var text = "world"
+console.log("Hello %s", text);
